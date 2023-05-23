259c9db1eafa1fa5a6dd3160e291a61d885373a9
BIGKINGS10 <adventbigkings@gmail.com>
2023-05-23 19:35:00 +0100

    Fixed the id param capture on product route

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Product Details for {{ id }}</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum cupiditate accusamus necessitatibus nulla, repellendus nisi velit fugiat ipsa laudantium delectus.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+</script>
+
+<style scoped>
+
+</style>

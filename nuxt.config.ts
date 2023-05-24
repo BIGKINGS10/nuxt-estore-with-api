18473b62cef127447538b228d10eb760d73d3bf8
BIGKINGS10 <adventbigkings@gmail.com>
2023-05-24 11:33:47 +0100

    Add material ui link and add to cart button on each product view

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'Nuxt e-Store',
+            meta: [
+                { name: 'description', content: 'Nuxt 3 Js e-store project'}
+            ],
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+            ]
+        }
+    }
 
 })

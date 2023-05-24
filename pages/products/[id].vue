600c3277570ab2f0110741a91a8e920aacafa2e5
BIGKINGS10 <adventbigkings@gmail.com>
2023-05-24 11:26:30 +0100

    completed error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,10 +9,14 @@
 <script setup>
     const { id } = useRoute().params
     const uri = 'https://fakestoreapi.com/products/' + id
-
+    
     //fetch the product
 
     const {data: product } = await useFetch(uri, { key: id })
+
+    if(!product.value){
+        throw createError({ statusCode: 400, statusMessage: 'Product not found', fatal: true})
+    }
 
     definePageMeta({
         layout: 'products'

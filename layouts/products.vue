d02d3c327090cc4f8241f07e761db1c40fbe87b3
BIGKINGS10 <adventbigkings@gmail.com>
2023-05-31 21:08:03 +0100

    Added a tracker page that usings useInterval

--- layouts/products.vue
+++ layouts/products.vue
@@ -2,7 +2,7 @@
     <div>
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4">
-                <NuxtLin to="/products" class="font-bold">HOME</NuxtLin>
+                <NuxtLink to="/products" class="font-bold">HOME</NuxtLink>
                
             </nav>
         </header>
---
+++ pages/tracker.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>Tracker</h2>
+        <p>
+            {{ data.thelast.sensorId }} <br />
+            {{ data.thelast.location }} <br />
+            {{ data.thelast.longitude }} <br />
+            {{ data.thelast.latitude }} <br />
+            {{ data.thelast.created_at }} <br />
+            {{ counter }}
+            
+        </p>
+
+      
+    </div>
+</template>
+
+<script setup>
+    import { useInterval } from '@vueuse/core'
+    const counter = useInterval(5000);
+    const { data } = await useFetch('https://www.osagie.dev/api/lastplace', { watch: [counter] });
+
+    
+
+    
+</script>
+
+<style scoped>
+
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px
+    }
+
+</style>
